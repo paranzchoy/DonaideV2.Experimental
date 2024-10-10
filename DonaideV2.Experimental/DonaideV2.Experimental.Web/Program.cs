58ce039879bd8a91c299c21f42b2496734d605fe
Franz Duran <engrparanz@gmail.com>
2024-10-10 12:04:38 +0800

    feat/Person: scaffold Person CRUD pages

--- DonaideV2.Experimental/DonaideV2.Experimental.Web/Program.cs
+++ DonaideV2.Experimental/DonaideV2.Experimental.Web/Program.cs
@@ -1,8 +1,17 @@
-using DonaideV2.Experimental.Shared.Services;
+﻿using DonaideV2.Experimental.Shared.Services;
 using DonaideV2.Experimental.Web.Components;
 using DonaideV2.Experimental.Web.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DonaideV2.Experimental.Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -24,6 +33,7 @@ else
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
