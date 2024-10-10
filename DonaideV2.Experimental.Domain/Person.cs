7663b58caebd7c9fc9366c26fc460205287e0719
Franz Duran <engrparanz@gmail.com>
2024-10-10 12:03:28 +0800

    project/Domain: create domain project with Person model

---
+++ DonaideV2.Experimental.Domain/Person.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DonaideV2.Experimental.Domain;
+
+[Table("Persons")]
+public class Person
+{
+    [Key]
+    public int PersonId { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(255)]
+    public string LastName { get; set; } = string.Empty;
+}
